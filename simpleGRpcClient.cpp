0cd7ed2597b886b6b1f47c531155558df6244520
moonyl <moonysl@hanmail.net>
2020-07-10 22:09:01 +0900

    echo 서비스 추가

--- simpleGRpcClient.cpp
+++ simpleGRpcClient.cpp
@@ -1,6 +1,38 @@
 #include <iostream>
 
+#include <grpcpp/channel.h>
+#include <grpcpp/create_channel.h>
+
+#include "simple_proto.grpc.pb.h"
+
+using grpc::Channel;
+using grpc::ClientContext;
+using grpc::Status;
+
+class SimpleGRpcClient {
+public:
+    SimpleGRpcClient(std::shared_ptr<Channel> channel) : stub_(SimpleGRpc::NewStub(channel)) {
+
+    }
+
+    void Echo(SimpleData reqData) {
+        SimpleData repData;
+        ClientContext context;
+        Status status = stub_->Echo(&context, reqData, &repData);
+        if (!status.ok()) {
+            std::cout << "Echo rpc failed." << std::endl;
+            return;
+        }
+        std::cout << "echo: " << repData.data() << std::endl;
+    }
+
+private:
+    std::unique_ptr<SimpleGRpc::Stub> stub_;
+};
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    SimpleGRpcClient client(grpc::CreateChannel("localhost:50051", grpc::InsecureChannelCredentials()));
+    SimpleData reqData;
+    reqData.set_data("my data");
+    client.Echo(reqData);
     return 0;
 }
--- simpleGRpcServer.cpp
+++ simpleGRpcServer.cpp
@@ -1,8 +1,35 @@
 #include <iostream>
+#include <grpcpp/server_builder.h>
 #include <simple_proto.grpc.pb.h>
 
-class SimpleGRpcServer final : public
+using grpc::Status;
+using grpc::ServerContext;
+using grpc::ServerBuilder;
+using grpc::Server;
+
+class SimpleGRpcServerImpl final : public SimpleGRpc::Service
+{
+public:
+    Status Echo(ServerContext *context, const SimpleData *request, SimpleData *response) override {
+        response->set_data(request->data());
+        return Status::OK;
+    }
+
+};
+
+void runServer() {
+    std::string serverAddress("0.0.0.0:50051");
+    SimpleGRpcServerImpl service;
+
+    ServerBuilder builder;
+    builder.AddListeningPort(serverAddress, grpc::InsecureServerCredentials());
+    builder.RegisterService(&service);
+    std::unique_ptr<Server> server(builder.BuildAndStart());
+    std::cout << "Server listening on " << serverAddress << std::endl;
+    server->Wait();
+}
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    //std::cout << "Hello, World!" << std::endl;
+    runServer();
     return 0;
 }
