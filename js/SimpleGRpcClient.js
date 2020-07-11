33a78634d2fc190fd9accb2d5a7c7d88debbb03c
moonyl <moonysl@hanmail.net>
2020-07-11 14:10:59 +0900

    javascript 버전 client

---
+++ js/SimpleGRpcClient.js
@@ -0,0 +1,28 @@
+const PROTO_PATH = __dirname + "/../protos/simple_proto.proto";
+
+const grpc = require("grpc");
+const protoLoader = require("@grpc/proto-loader");
+const packageDefinition = protoLoader.loadSync(PROTO_PATH, {
+  keepCase: true,
+  longs: String,
+  enums: String,
+  defaults: true,
+  oneofs: true,
+});
+
+//console.log(grpc.loadPackageDefinition(packageDefinition));
+const simpleProto = grpc.loadPackageDefinition(packageDefinition);
+
+const main = () => {
+  //console.log(simpleProto);
+  const client = new simpleProto.SimpleGRpc(
+    "localhost:50051",
+    grpc.credentials.createInsecure()
+  );
+
+  client.Echo({ data: "from js" }, (err, response) => {
+    console.log("Echo: ", response.data);
+  });
+};
+
+main();
